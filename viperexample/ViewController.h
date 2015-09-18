a8a7ee4817119fc46a726ee4921afa180fe7ccb5
Igor Lipovac <igor.lipovac@infinum.hr>
2015-09-18 11:21:58 +0200

    Initial Commit

---
+++ viperexample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  viperexample
+//
+//  Created by Igor Lipovac on 18/09/15.
+//  Copyright (c) 2015 Igor Lipovac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ viperexample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  viperexample
+//
+//  Created by Igor Lipovac on 18/09/15.
+//  Copyright (c) 2015 Igor Lipovac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
